05cb68f3bf6b842fda8fc25d2f0e5ade1e734885
Yin Jifeng <jifeng.yin@gmail.com>
2016-02-28 23:12:56 +0800

    test data

---
+++ server/fixtures/discount.sql
@@ -0,0 +1,3 @@
+INSERT INTO discount (id, discounttype, disabled, productids) VALUES
+(1, 1, "", "[3]"),
+(2, 2, "[1]", "[1,2,3]");
---
+++ server/fixtures/product.sql
@@ -0,0 +1,4 @@
+INSERT INTO product (id, price, name, unitname) VALUES
+(1, 3, "可口可乐", "瓶"),
+(2, 1, "羽毛球", "个"),
+(3, 5.5, "苹果", "斤");
